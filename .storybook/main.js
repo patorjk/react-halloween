3c4c25181c52075670a679ab1b8bdfd98e662bea
Patrick Gillespie <patorjk@gmail.com>
2022-11-03 23:49:55 -0400

    Added storybook config

---
+++ .storybook/main.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      type: "javascript/auto",
+      test: /\.mjs$/,
+      include: /node_modules/,
+    });
+
+    // Return the altered config
+    return config;
+  },
+  "framework": "@storybook/react"
+}

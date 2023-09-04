a4047cd893da26fca621950de7ef09c5d19ce251
patorjk <patorjk@gmail.com>
2023-09-04 15:32:04 -0400

    test fix

--- src/components/Haunted/Haunted.test.jsx
+++ src/components/Haunted/Haunted.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import Haunted from './Haunted';
+import { Haunted } from './Haunted';
 
 // TODO: TESTS WILL COME AT VERSION 1.0.0
 describe('Haunted', () => {

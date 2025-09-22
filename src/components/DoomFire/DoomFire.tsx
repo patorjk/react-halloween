47c03134ffa759bd0b4df4c2aaad1f3e264fb49c
patorjk <patorjk@gmail.com>
2025-09-21 21:18:00 -0400

    adjustments

--- src/components/DoomFire/DoomFire.tsx
+++ src/components/DoomFire/DoomFire.tsx
@@ -1,11 +1,31 @@
 import React, { useEffect, useMemo, useRef } from 'react';
-import { fadeColors, type RGBAColor } from 'color-fader';
+import { fadeColors, type RgbaColor } from 'color-fader';
 
-// Get pixel index in 1D array from x,y coordinates
-function getPixelIndex(x: number, y: number, width: number) {
-  return y * width + x;
-}
+/**
+ * Algorithm that explains Doom Fire is talked about here:
+ * https://fabiensanglard.net/doom_fire_psx/
+ */
 
+/**
+ * Gets an index into the 1D array that represents the fire.
+ *
+ * @param x {number} X position.
+ * @param y {number} Y position.
+ * @param width {number} Width of the fire.
+ */
+const getPixelIndex = (x: number, y: number, width: number) => y * width + x;
+
+/**
+ * Takes in an existing fireBuffer and creates the next iteration
+ *
+ * @param fireBuffer {number[]} 1D array representing a 2D plane. Top-left pixel is at array index zero
+ *   and bottom-right pixel is at index height*width-1.
+ * @param width {number} Width of the fire in pixels.
+ * @param height {number} Height of hte fire in pixels.
+ * @param maxIntensity {number} Number indicating the intensity. Higher the number, higher the intensity. Max height of
+ *   the fire is the max intensity.
+ * @param fireEnabled {boolean} Toggling on/off leads to a mini animation.
+ */
 function updateFire(
   fireBuffer: number[],
   width: number,
@@ -25,7 +45,7 @@ function updateFire(
         newFireBuffer[pixelIndex] = 0;
       } else {
         // Add randomness to create flickering effect
-        const randomDecay = Math.floor(Math.random() * 3);
+        const randomDecay = Math.floor(Math.random() * 3); // 0, 1, or 2
         const randomWind = Math.floor(Math.random() * 3) - 1; // -1, 0, or 1
 
         const newIntensity = fireIntensity - randomDecay;
@@ -37,20 +57,38 @@ function updateFire(
     }
   }
 
-  // Maintain fire source at bottom if fire is enabled
-  if (fireEnabled) {
-    for (let x = 0; x < width; x++) {
+  for (let x = 0; x < width; x++) {
+    if (fireEnabled) {
+      // Maintain fire source at bottom if fire is enabled
       newFireBuffer[getPixelIndex(x, height - 1, width)] = maxIntensity;
+    } else {
+      // extinguish the fire by killing the bottom source
+      newFireBuffer[getPixelIndex(x, height - 1, width)] = Math.max(
+        0,
+        newFireBuffer[getPixelIndex(x, height - 1, width)] - Math.floor(Math.random() * 3),
+      );
     }
   }
+
   return newFireBuffer;
 }
 
+/**
+ * Draws the fires onto the canvas.
+ *
+ * @param canvas {HTMLCanvasElement} Canvas that will hold the fire.
+ * @param ctx {CanvasRenderingContext2D} Canvas context of the canvas.
+ * @param fireBuffer {number[]} 1D array representing a 2D plane. Top-left pixel is at array index zero
+ *   and bottom-right pixel is at index height*width-1.
+ * @param firePalette {RgbaColor[]} An array of color values that map to a fire intensity.
+ * @param fireWidth {number} Width of fire.
+ * @param fireHeight {number} Height of fire.
+ */
 function renderFire(
+  canvas: HTMLCanvasElement,
   ctx: CanvasRenderingContext2D,
   fireBuffer: number[],
-  firePalette: RGBAColor[],
-  canvas: HTMLCanvasElement,
+  firePalette: RgbaColor[],
   fireWidth: number,
   fireHeight: number,
 ) {
@@ -59,14 +97,15 @@ function renderFire(
   for (let y = 0; y < fireHeight; y++) {
     for (let x = 0; x < fireWidth; x++) {
       const pixelIndex = getPixelIndex(x, y, fireWidth);
-      const fireIntensity = fireBuffer[pixelIndex];
+      const fireIntensity = Math.min(fireBuffer[pixelIndex], firePalette.length - 1);
+
       const color = firePalette[fireIntensity];
 
       const imageIndex = (y * fireWidth + x) * 4;
-      imageData.data[imageIndex] = color.r; // Red
-      imageData.data[imageIndex + 1] = color.g; // Green
-      imageData.data[imageIndex + 2] = color.b; // Blue
-      imageData.data[imageIndex + 3] = color.a * 255; // Alpha
+      imageData.data[imageIndex] = color.r;
+      imageData.data[imageIndex + 1] = color.g;
+      imageData.data[imageIndex + 2] = color.b;
+      imageData.data[imageIndex + 3] = color.a * 255;
     }
   }
 
@@ -87,41 +126,90 @@ function renderFire(
 function initFire(width: number, height: number, maxIntensity: number = 36): number[] {
   const fireBuffer = Array(width * height).fill(0);
 
-  // Set bottom row to maximum fire intensity
+  // While fire is on, bottom row should be at maximum fire intensity
   for (let x = 0; x < width; x++) {
     fireBuffer[getPixelIndex(x, height - 1, width)] = maxIntensity;
   }
   return fireBuffer;
 }
 
-const fireColors = ['#771f0700', '#771f0700', '#771f07', '#DF4F07', '#cf770f', '#BF9F1F', '#B7B72F', '#ffffff'];
+const defaultFireColors = ['#771f0700', '#771f07', '#DF4F07', '#cf770f', '#BF9F1F', '#B7B72F', '#ffffff'];
 
-interface DoomFireProps {
+function usePrevious(value) {
+  const ref = useRef<number>(undefined);
+
+  useEffect(() => {
+    ref.current = value; // Update the ref's current value after every render
+  }, [value]); // Rerun this effect only when 'value' changes
+
+  return ref.current; // Return the value from the previous render
+}
+
+export interface DoomFireProps {
   height?: number;
   width?: number;
   pixelSize?: number;
+  fireColors?: string[];
+  fireEnabled?: boolean;
+  fireStrength?: number;
 }
 
-const DoomFire = ({ height = 300, width = 400, pixelSize = 2 }: DoomFireProps) => {
+/**
+ *
+ * @param height {number} Height of canvas in pixels.
+ * @param width {number} Width of canvas in pixels.
+ * @param pixelSize {number} How big a pixel should be in the output. Larger means more pixelated.
+ * @param fireColors {string[]} Array of CSS color values.
+ * @param fireEnabled {boolean} If the fire is on.
+ * @param fireStrength {number} Number between 0 and 1, represents the "strength" of the fire.
+ * @constructor
+ */
+const DoomFire = ({
+  height = 300,
+  width = 400,
+  pixelSize = 2,
+  fireColors = defaultFireColors,
+  fireEnabled = true,
+  fireStrength = 0.75,
+}: DoomFireProps) => {
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
 
   const fireWidth = Math.ceil(width / pixelSize);
   const fireHeight = Math.ceil(height / pixelSize);
 
-  const flamePalette = useMemo(() => fadeColors(fireColors, fireHeight), [fireHeight]);
+  const prevStrength = usePrevious(fireStrength);
+  const prevFireWidth = usePrevious(fireWidth);
+  const prevFireHeight = usePrevious(fireHeight);
 
-  const fireBufferRef = useRef(initFire(fireWidth, fireHeight, flamePalette.length - 1));
+  const fireBufferRef = useRef(null);
 
+  const flamePalette: RgbaColor[] = useMemo(() => {
+    const fadedColors = fadeColors(fireColors ?? defaultFireColors, fireHeight * fireStrength);
+    if (fireStrength !== prevStrength || prevFireWidth !== fireWidth || prevFireHeight !== fireHeight) {
+      fireBufferRef.current = initFire(fireWidth, fireHeight, fadedColors.length - 1);
+    }
+    return fadedColors;
+  }, [fireColors, fireWidth, fireHeight, fireStrength, prevStrength, prevFireWidth, prevFireHeight]);
+
+  /**
+   * This effect governs the animation.
+   */
   useEffect(() => {
     let animationFrameId;
 
     const animate = () => {
-      fireBufferRef.current = updateFire(fireBufferRef.current, fireWidth, fireHeight, flamePalette.length - 1);
+      fireBufferRef.current = updateFire(
+        fireBufferRef.current,
+        fireWidth,
+        fireHeight,
+        flamePalette.length - 1,
+        fireEnabled,
+      );
       renderFire(
+        canvasRef.current,
         canvasRef.current.getContext('2d'),
         fireBufferRef.current,
         flamePalette,
-        canvasRef.current,
         fireWidth,
         fireHeight,
       );
@@ -130,11 +218,11 @@ const DoomFire = ({ height = 300, width = 400, pixelSize = 2 }: DoomFireProps) =
 
     animationFrameId = requestAnimationFrame(animate);
 
-    // Cleanup function to cancel the animation
+    // Cleanup
     return () => {
       cancelAnimationFrame(animationFrameId);
     };
-  }, [fireWidth, fireHeight, flamePalette]);
+  }, [fireWidth, fireHeight, flamePalette, fireEnabled]);
 
   return <canvas id="doomfire" width={`${width}px`} height={`${height}px`} ref={canvasRef} />;
 };
--- src/stories/DoomFire.stories.tsx
+++ src/stories/DoomFire.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { type ChangeEvent, useCallback, useMemo, useState } from 'react';
 
 import { DoomFire } from '../components';
 
@@ -11,13 +11,63 @@ export default {
   },
 };
 
+const purpleRedFireColor = [
+  '#FF000000', // transparent
+  '#FF0000', // Red
+  'purple',
+];
+
+const greenFireColors = [
+  '#66FF0000', // transparent
+  '#66FF00',
+  '#097969',
+];
+
+const yellowRedFireColors = ['#FF634700', '#FF6347', '#FFD700'];
+
+const purpleFireColors = ['#FF69B400', '#FF69B4', '#BA55D3'];
+
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-function Template(args) {
+function Template() {
   const [loKey, setLoKey] = useState(0);
+  const [enabled, setEnabled] = useState(true);
 
   const updateKey = () => {
     setLoKey(loKey + 1);
   };
+  const toggleEnabled = () => setEnabled((prev) => !prev);
+
+  const [firePixelation, setFirePixelation] = useState(1);
+  const handlePixelationChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setFirePixelation(Number(event.target.value));
+  }, []);
+
+  const [fireStrength, setFireStrength] = useState(0.75);
+  const handleFireStrengthChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setFireStrength(Number(event.target.value));
+  }, []);
+
+  const [colorSelectValue, setColorSelectValue] = useState('default');
+
+  const colors = useMemo(() => {
+    if (colorSelectValue === 'purpleRedFireColor') {
+      return purpleRedFireColor;
+    }
+    if (colorSelectValue === 'greenFireColors') {
+      return greenFireColors;
+    }
+    if (colorSelectValue === 'yellowRedFireColors') {
+      return yellowRedFireColors;
+    }
+    if (colorSelectValue === 'purpleFireColors') {
+      return purpleFireColors;
+    }
+    return null;
+  }, [colorSelectValue]);
+
+  const handleColorChnage = useCallback((event: ChangeEvent<HTMLSelectElement>) => {
+    setColorSelectValue(event.target.value);
+  }, []);
 
   return (
     <div
@@ -33,23 +83,63 @@ function Template(args) {
         gap: '1rem',
       }}
     >
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <DoomFire key={loKey} {...args} />
-      <button type="button" onClick={updateKey}>
-        Reset
-      </button>
+      <DoomFire
+        key={loKey}
+        fireEnabled={enabled}
+        fireColors={colors}
+        pixelSize={firePixelation}
+        fireStrength={fireStrength}
+      />
+      <div style={{ display: 'flex', gap: '1rem' }}>
+        <button type="button" onClick={updateKey}>
+          Reset
+        </button>
+        <button type="button" onClick={toggleEnabled}>
+          {enabled ? 'Disable' : 'Enable'}
+        </button>
+      </div>
+      <div style={{ display: 'flex', gap: '0.5rem' }}>
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label htmlFor="fire-colors">Fire Colors</label>
+        <select id="fire-colors" name="fire-colors" value={colorSelectValue} onChange={handleColorChnage}>
+          <option value="default">Default</option>
+          <option value="purpleRedFireColor">Purple to Red Fire</option>
+          <option value="greenFireColors">Green Fire</option>
+          <option value="yellowRedFireColors">Yellow Red Fire</option>
+          <option value="purpleFireColors">Purple Pink Fire</option>
+        </select>
+      </div>
+      <div style={{ display: 'flex', gap: '0.5rem' }}>
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label htmlFor="fire-pixelation">Fire Pixelation</label>
+        <input
+          type="range"
+          id="fire-pixelation"
+          name="fire-pixelation"
+          min="1"
+          max="10"
+          value={firePixelation}
+          onChange={handlePixelationChange}
+        />
+      </div>
+      <div style={{ display: 'flex', gap: '0.5rem' }}>
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label htmlFor="fire-strength">Fire Strength</label>
+        <input
+          type="range"
+          id="fire-strength"
+          name="fire-strength"
+          min="0.1"
+          max="1"
+          step="0.05"
+          value={fireStrength}
+          onChange={handleFireStrengthChange}
+        />
+      </div>
     </div>
   );
 }
 
 export const Simple = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Simple.args = {
-  size: 300,
-};
-
-export const NoLightsOn = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-NoLightsOn.args = {
-  clickToTurnOnLights: false,
-};
+Simple.args = {};
